5ae9f9036231f2bdd3ca694fcddb9bc27de7a05e
edina.kim <edina.kim@daumkakao.com>
2016-07-27 14:31:26 +0900

    scalatest wordSpec -> FunSuite

--- src/test/scala/sectionsnote/AssertsSpec.scala
+++ src/test/scala/sectionsnote/AssertsSpec.scala
@@ -1,20 +1,23 @@
 package sectionsnote
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{FunSuite, Matchers}
 
 /**
   * Created by edina on 2016. 7. 21..
+  * http://www.scalatest.org/user_guide
   */
-class AssertsSpec extends WordSpec with Matchers {
-  "asserts group test" should {
-    "matchers" in {
-      true should be(true)
+class AssertsSpec extends FunSuite with Matchers {
 
-      val v1 = 4
-      v1 shouldEqual 4
+  test("hello scala") {
+    println("hello world!")
+  }
 
-      assert(2 == 1+1)
+  test("asserts test") {
+    true should be(true)
 
-    }
+    val v1 = 4
+    v1 shouldEqual 4
+
+    assert(2 == 1+1)
   }
 }
